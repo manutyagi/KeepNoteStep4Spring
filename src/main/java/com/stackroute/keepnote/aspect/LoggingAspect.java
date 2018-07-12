ee2708419792c9619f5a68aa0e1c901475d310c4
Rutuja.Bacchuwar <rutuja.bacchuwar@stackroute.in>
2018-07-12 09:38:47 +0530

    step-4

---
+++ src/main/java/com/stackroute/keepnote/aspect/LoggingAspect.java
@@ -0,0 +1,12 @@
+package com.stackroute.keepnote.aspect;
+
+/* Annotate this class with @Aspect and @Component */
+
+public class LoggingAspect {
+
+	/*
+	 * Write loggers for each of the methods of controller, any particular method
+	 * will have all the four aspectJ annotation
+	 * (@Before, @After, @AfterReturning, @AfterThrowing).
+	 */
+}
